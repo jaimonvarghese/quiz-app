e2a61b9b14d5a260fe29307c7fa06cbbd594b583
Jaimon Varghese <jaimonvarghese1224@gmail.com>
2025-03-06 23:04:15 +0530

    add a custom widet for answer button for reuseing

---
+++ lib/widgets/answer_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({super.key, required this.anserText, required this.onTap});
+
+  final String anserText;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(vertical: 10,horizontal: 50),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+        backgroundColor: const Color.fromARGB(255, 21, 15, 15),
+        foregroundColor: const Color.fromARGB(255, 239, 236, 236),
+      ),
+      onPressed: onTap,
+      child: Text(anserText,textAlign: TextAlign.center,),
+    );
+  }
+}
--- lib/widgets/question_screen.dart
+++ lib/widgets/question_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/widgets/answer_button.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -8,20 +10,39 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  var currentQuestionIndex = 0;
+  void answerQuestion (){
+    setState(() {
+      currentQuestionIndex ++;
+    });
+  }
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[currentQuestionIndex];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('The Question..',),
-          const SizedBox(height: 30,),
-          ElevatedButton(onPressed: (){}, child: Text("Answer2"),),
-          ElevatedButton(onPressed: (){}, child: Text("Answer2"),),
-          ElevatedButton(onPressed: (){}, child: Text("Answer2"),),
-          ElevatedButton(onPressed: (){}, child: Text("Answer2"),),
-        ],
+      child: Container(
+        margin: EdgeInsets.all(40),
+        child: Column(
+        
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(anserText: answer, onTap:answerQuestion);
+            }),
+          ],
+        ),
       ),
     );
   }
